556e3d4132a7f2dd8eb06fd25e28cd36e07cc2cf
Marina Golacheva <marina.golacheva@gmail.com>
2022-04-12 14:13:13 +0200

    smaller components, finished styling menu-item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'; 
 import './App.css';
-import HomePage from './homepage.components';
-import './homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss';
+
+
 
 function App() {
   return <div><HomePage/></div>;
